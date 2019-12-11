dce5f811bc9d96a26c8508c56e8848fda8cd84be
jumpserver01 <koh@koh.gg>
2019-12-11 11:50:24 -0600

    update examples

--- examples/mockapi/raw_terraform/main.tf
+++ examples/mockapi/raw_terraform/main.tf
@@ -5,7 +5,7 @@ resource "restful_rest_call" "GET1" {
   method = "GET"
   uri = "http://localhost:8080/events/1"
   expected_response_code = 200
-  expected_response_body = "{\"ID\":\"1\",\"Title\":\"Introduction to Golang\",\"Description\":\"Come join us for a chance to learn how golang works and get to eventually try it out\"}"
+  expected_response_body = "[{\"ID\":\"1\",\"Title\":\"Introduction to Golang\",\"Description\":\"Come join us for a chance to learn how golang works and get to eventually try it out\"}]"
   json_key_outputs = ["Title", "Description"]
 }
 //GET expected_response_body is in the response body but response body contains more data
@@ -16,12 +16,6 @@ resource "restful_rest_call" "GET2" {
 }
 //GET a specific item
 
-resource "restful_rest_call" "DELETE1" {
-  method = "DELETE"
-  uri = "http://localhost:8080/events/2"
-  expected_response_code = 200
-  depends_on = [restful_rest_call.GET3]
-}
 resource "restful_rest_call" "POST1" {
   method = "POST"
   uri = "http://localhost:8080/event"
@@ -29,6 +23,12 @@ resource "restful_rest_call" "POST1" {
   request_body = "{\"ID\":\"2\",\"Title\":\"title\",\"Description\":\"desc2\"}"
   expected_response_body = "{\"ID\":\"2\",\"Title\":\"title\",\"Description\":\"desc2\"}"
   expected_response_code = 201
+}
+resource "restful_rest_call" "DELETE1" {
+  method = "DELETE"
+  uri = "http://localhost:8080/events/2"
+  expected_response_code = 200
+  depends_on = [restful_rest_call.GET3]
 }
 resource "restful_rest_call" "PATCH1" {
   method = "PATCH"
@@ -45,5 +45,6 @@ resource "restful_rest_call" "GET3" {
   expected_response_body = "{\"ID\":\"2\",\"Title\":\"title\",\"Description\":\"desc3\"}"
   depends_on = [restful_rest_call.PATCH1]
 }
+
 
 
